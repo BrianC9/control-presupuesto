7a66512191c43dc8686d00cfd2b375a8b6fdd97b
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-07-12 17:17:28 +0200

    boilerplate with img folder and resources

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import casa from './img/icono_casa.svg';
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+      <img src={casa} width={50} />
+    </div>
+  );
+}
+
+export default App;
