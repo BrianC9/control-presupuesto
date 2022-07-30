3df69202b4e8cd6b04081dac0e7fc5f311b19e59
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-07-30 20:06:03 +0200

    starting with the swipe functionality

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,4 +1,11 @@
-import React from 'react';
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from 'react-swipeable-list';
+import 'react-swipeable-list/dist/styles.css';
 import { capitalizeFirstLetter } from '../utils/index.js';
 import IconoAhorro from '../img/icono_ahorro.svg';
 import IconoComida from '../img/icono_comida.svg';
@@ -19,21 +26,42 @@ const iconsCategoria = {
 };
 function Gasto({ gasto }) {
   const { cantidad, concepto, categoria, fecha } = gasto;
+
+  const leadingActions = () => (
+    <LeadingActions>
+      <SwipeAction onClick={() => console.log('editar...')}>Editar</SwipeAction>
+    </LeadingActions>
+  );
+
+  const trailingActions = () => (
+    <TrailingActions>
+      <SwipeAction onClick={() => console.log('eliminar...')}>
+        Eliminar
+      </SwipeAction>
+    </TrailingActions>
+  );
+
   return (
-    <div className='gasto sombra'>
-      <div className='contenido-gasto'>
-        <img src={iconsCategoria[categoria]} />
-        {console.log(`Icono${capitalizeFirstLetter(categoria)}`)}
-        <div className='descripcion-gasto'>
-          <p className='categoria'>{categoria}</p>
-          <p className='nombre-gasto'>{capitalizeFirstLetter(concepto)}</p>
-          <p className='fecha-gasto'>
-            <span>{fecha}</span>{' '}
-          </p>
+    <SwipeableList>
+      <SwipeableListItem
+        leadingActions={leadingActions()}
+        trailingActions={trailingActions()}
+      >
+        <div className='gasto sombra'>
+          <div className='contenido-gasto'>
+            <img src={iconsCategoria[categoria]} />
+            <div className='descripcion-gasto'>
+              <p className='categoria'>{categoria}</p>
+              <p className='nombre-gasto'>{capitalizeFirstLetter(concepto)}</p>
+              <p className='fecha-gasto'>
+                <span>{fecha}</span>{' '}
+              </p>
+            </div>
+          </div>
+          <p className='cantidad-gasto'>€ {cantidad} </p>
         </div>
-      </div>
-      <p className='cantidad-gasto'>€ {cantidad} </p>
-    </div>
+      </SwipeableListItem>
+    </SwipeableList>
   );
 }
 
